6b2ce6a40380640e32fa2336a364e8acfc21261a
JoanAlejo <jamunozb@udistrital.edu.co>
2026-05-21 20:38:27 -0500

    Creación metodos getters setters y tostring de Moto

--- src/Modelado/Moto.java
+++ src/Modelado/Moto.java
@@ -16,4 +16,63 @@ public class Moto {
         this.precio = precio;
         this.tipo = tipo;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(int cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto{" +
+                "id=" + id +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", cilindraje=" + cilindraje +
+                ", precio=" + precio +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
 }
