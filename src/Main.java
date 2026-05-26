c3a5c042670c36d79f6870182aa1d2f2c91b54f0
JoanAlejo <jamunozb@udistrital.edu.co>
2026-05-26 12:54:38 -0500

    Creacion de la estructura del main para que sea un menu infito y limpio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import menu.Menu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        Menu mn = new Menu();
+        int x=1;//Creamos la variable donde guardaremos si el usuario quiere terminar el programa
+        while (x!=0){
+            x=mn.mostrarMenu();//el metodo mostrarMenu se ejecuta devuelve un entero que de ser 0 el usuario quiere terminar la ejecucion
         }
     }
+}
